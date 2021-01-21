b5bd8ff19fbcf741fb3268496dd3926f018b8476
Javier Cardenas <glatorian@gmail.com>
2021-01-20 19:46:54 -0500

    shuffle array funtion

--- assets/js/ApiTest.js
+++ assets/js/ApiTest.js
@@ -9,51 +9,65 @@ var lfmartist = "the beatles".split(' ').join('+') //$("#example-input").split('
 
 //$(document).ready(function(){
 //function searchQueryURLSimArt() {
-    var qURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=2&artist=" + lfmartist + "&autocorrect[1]&api_key=" + lastFmKey + "&format=json"
-    $.ajax({
-        url: qURL,
-        method: "GET"
-    }).then(function (lfmResponse) {
-        console.log(qURL);
-        console.log(lfmResponse)
+var qURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=2&artist=" + lfmartist + "&autocorrect[1]&api_key=" + lastFmKey + "&format=json";
+$.ajax({
+    url: qURL,
+    method: "GET"
+}).then(function (lfmResponse) {
+    console.log(qURL);
+    console.log(lfmResponse)
 
-       //get length of array and iterate to get all artists in array
-       var lfmArtistNameArray = lfmResponse.similarartists.artist 
+    //get length of array and iterate to get all artists in array
+    var lfmArtistNameArray = lfmResponse.similarartists.artist
 
-        //create array to send to youtube API
-        var ytubeTerm = lfmArtistNameArray.map(function(ytubeTermIn) {
-            return ytubeTermIn.name;
-        });
-        //test to see if array works
-        //console.log(ytubeTerm);
-        //save to local storage
-        localStorage.setItem("ytubeTerm", JSON.stringify(ytubeTerm));
+    //create array to send to youtube API
+    var ytubeTerm = lfmArtistNameArray.map(function (ytubeTermIn) {
+        return ytubeTermIn.name;
     });
-    //retrive array and test
-    var ytubeSTerm = JSON.parse(localStorage.getItem("ytubeTerm"));
-  //  console.log(ytubeSTerm);
+    //test to see if array works
+    //console.log(ytubeTerm);
+    //save to local storage
+    localStorage.setItem("ytubeTerm", JSON.stringify(ytubeTerm));
+});
+//retrive array and test
+var ytubeSTerm = JSON.parse(localStorage.getItem("ytubeTerm"));
 
 //}
 //console.log(searchQueryURLSimArt);
 
+//shuffle array 1/20/21 
+function shuffleArray(ytubeSTerm) {
+    for (let i = ytubeSTerm.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [ytubeSTerm[i], ytubeSTerm[j]] = [ytubeSTerm[j], ytubeSTerm[i]];
+    }
+};
+shuffleArray(ytubeSTerm);
+
+//test to see if it shuffles
+console.log(ytubeSTerm);
+
 
 //Youtube api seperate component, can be moved
-ytubeSTerm.forEach(function(yQueryLoop) {
-console.log(yQueryLoop);
-var qURLy = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + yQueryLoop.split(' ').join('+') + "&type=video&key=" + youtubeKey
-$.ajax({
-    url: qURLy,
-    method: "GET"
-}).then(function (ytResponse) {
-    console.log(qURLy);
-    console.log(ytResponse);
+ytubeSTerm.forEach(function (yQueryLoop) {
+    console.log(yQueryLoop);
+    var qURLy = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + yQueryLoop.split(' ').join('+') + "&type=video&key=" + youtubeKey
+    $.ajax({
+        url: qURLy,
+        method: "GET"
+    }).then(function (ytResponse) {
+        console.log(qURLy);
+        console.log(ytResponse);
 
-    //successfully retrives videoId
-    var ytReturnVidId = ytResponse.items[0].id.videoId
-    console.log(ytReturnVidId);
+        //successfully retrives videoId
+        var ytReturnVidId = ytResponse.items[0].id.videoId
+        console.log(ytReturnVidId);
 
-    //example of JSON syntax for youtube API, video title 
-    var ytReturnVidTitle = ytResponse.items[0].snippet.title
-    console.log(ytReturnVidTitle);
+        //example of JSON syntax for youtube API, video title 
+        var ytReturnVidTitle = ytResponse.items[0].snippet.title
+        console.log(ytReturnVidTitle);
+    }).then(function (appendYtHtml) {
+
+    });
 });
-});
+
