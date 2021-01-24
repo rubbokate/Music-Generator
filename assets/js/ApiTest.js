dd6be98edf29f5261f1f1d6636f9eca8bf218df9
Glatorian13 <glatorian13@gmail.com>
2021-01-24 17:54:38 -0500

    js and html, no big changes from last commit -J

--- assets/js/ApiTest.js
+++ assets/js/ApiTest.js
@@ -3,8 +3,98 @@ const lastFmKey = "3b2324e54073b3dc0b3f4e2407ba58d1"
 const lastFmSecret = "82409a163bce82966c42be3a35f3d950"
 const youtubeKey = "AIzaSyAaQzZrnuJSEVUnyYXGYHcEKoluy22eyu0"
 
-//for artist search need + instead of " " --> loop?
-var lfmartist = "the beatles".split(' ').join('+') //$("#example-input").split(' ').join('+')
+
+
+$(document).ready(function(){
+    //for artist search need + instead of " " --> loop?
+    //var lfmartist = $(".input");  //"the beatles" //$(".input")
+    //need to work on jquery to save input from form and submit on btn press
+    var lfmartist = "the beatles"
+   // console.log(lfmartist);
+
+
+//function searchQueryURLSimArt() {
+var qURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=2&artist=" + lfmartist.split(' ').join('+') + "&autocorrect[1]&api_key=" + lastFmKey + "&format=json"
+$.ajax({
+    url: qURL,
+    method: "GET"
+}).then(function (lfmResponse) {
+    console.log(qURL);
+    console.log(lfmResponse)
+
+    //get length of array and iterate to get all artists in array
+    var lfmArtistNameArray = lfmResponse.similarartists.artist
+
+    //create array to send to youtube API
+    var ytubeTerm = lfmArtistNameArray.map(function (ytubeTermIn) {
+        return ytubeTermIn.name;
+    });
+    //test to see if array works
+    //console.log(ytubeTerm);
+    //save to local storage
+    localStorage.setItem("ytubeTerm", JSON.stringify(ytubeTerm));
+});
+//retrive array and test
+
+//UNCOMMENT ME LATER
+var ytubeSTerm = JSON.parse(localStorage.getItem("ytubeTerm"));
+
+//}
+//console.log(searchQueryURLSimArt);
+
+//shuffle array 1/20/21 
+function shuffleArray(ytubeSTerm) {
+    for (let i = ytubeSTerm.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [ytubeSTerm[i], ytubeSTerm[j]] = [ytubeSTerm[j], ytubeSTerm[i]];
+    }
+};
+shuffleArray(ytubeSTerm);
+
+//test to see if it shuffles
+//console.log(ytubeSTerm);
+
+
+//Youtube api seperate component, can be moved
+ytubeSTerm.forEach(function (yQueryLoop) {
+    console.log(yQueryLoop);
+    var qURLy = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + yQueryLoop.split(' ').join('+') + "&type=video&key=" + youtubeKey
+    $.ajax({
+        url: qURLy,
+        method: "GET"
+    }).then(function (ytResponse) {
+        console.log(qURLy);
+        console.log(ytResponse);
+
+        //successfully retrives videoId
+        var ytReturnVidId = ytResponse.items[0].id.videoId
+        console.log(ytReturnVidId);
+
+        //placeholder for video id 
+        //var ytReturnVidId = "1V_xRb0x9aw"
+
+        //retrieve video title 
+        var ytReturnVidTitle = ytResponse.items[0].snippet.title
+        console.log(ytReturnVidTitle);
+        
+        //retrieve video thumbnails
+        var ytReturnThumb = ytResponse.items[0].snippet.thumbnails.default
+        console.log(ytReturnThumb);
+
+        var comboResults = `<li> <a href="https://www.youtube.com/watch?v=${ytReturnVidId}" target="_blank" rel="noopener noreferrer">${ytReturnVidTitle}</a></li>`
+
+        //append to document with above results, use comboResults as inside html text
+        $("#ytResults").append(comboResults);
+        
+
+        var dummyTest = "Test is success"
+        
+    });
+
+});
+    console.log(dummyTest);
+});
+
 
 //YOUTUBE
 var tag = document.createElement('script');
@@ -49,88 +139,3 @@ function stopVideo() {
 }
 
 //YOUTUBE
-
-
-//$(document).ready(function(){
-//function searchQueryURLSimArt() {
-var qURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&limit=2&artist=" + lfmartist + "&autocorrect[1]&api_key=" + lastFmKey + "&format=json"
-$.ajax({
-    url: qURL,
-    method: "GET"
-}).then(function (lfmResponse) {
-    console.log(qURL);
-    console.log(lfmResponse)
-
-    //get length of array and iterate to get all artists in array
-    var lfmArtistNameArray = lfmResponse.similarartists.artist
-
-    //create array to send to youtube API
-    var ytubeTerm = lfmArtistNameArray.map(function (ytubeTermIn) {
-        return ytubeTermIn.name;
-    });
-    //test to see if array works
-    //console.log(ytubeTerm);
-    //save to local storage
-    localStorage.setItem("ytubeTerm", JSON.stringify(ytubeTerm));
-});
-//retrive array and test
-
-//UNCOMMENT ME LATER
-var ytubeSTerm = JSON.parse(localStorage.getItem("ytubeTerm"));
-
-//}
-//console.log(searchQueryURLSimArt);
-
-//test item for ytubeSTerm
-//let ytubeSTerm = ["A_MjCqQoLLA", "3L4YrGaR8E4","1V_xRb0x9aw"]
-
-
-//shuffle array 1/20/21 
-function shuffleArray(ytubeSTerm) {
-    for (let i = ytubeSTerm.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [ytubeSTerm[i], ytubeSTerm[j]] = [ytubeSTerm[j], ytubeSTerm[i]];
-    }
-};
-shuffleArray(ytubeSTerm);
-
-//test to see if it shuffles
-//console.log(ytubeSTerm);
-
-
-//Youtube api seperate component, can be moved
-ytubeSTerm.forEach(function (yQueryLoop) {
-    console.log(yQueryLoop);
-    var qURLy = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + yQueryLoop.split(' ').join('+') + "&type=video&key=" + youtubeKey
-    $.ajax({
-        url: qURLy,
-        method: "GET"
-    }).then(function (ytResponse) {
-        console.log(qURLy);
-        console.log(ytResponse);
-
-        //successfully retrives videoId
-        var ytReturnVidId = ytResponse.items[0].id.videoId
-        console.log(ytReturnVidId);
-
-        //retrieve video title 
-        var ytReturnVidTitle = ytResponse.items[0].snippet.title
-        console.log(ytReturnVidTitle);
-        
-        //retrieve video thumbnails
-        var ytReturnThumb = ytResponse.items[0].snippet.thumbnails.default
-        console.log(ytReturnThumb);
-
-        var comboResults = '<li> <a href="https://www.youtube.com/watch?v=' + ytReturnVidId + '">' + ytReturnVidTitle + '</a>' + '</li>'
-
-        //append to document with above results, use comboResults as inside html text
-        $("#ytResults").append(comboResults);
-        
-
-        var dummyTest = "Test is success"
-        
-    });
-
-});
-    console.log(dummyTest);
-
