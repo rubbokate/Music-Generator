cc3f9631680d38c22bd622e08d306d1f5d332e4d
Kaitlyn Rubbo <kaitlynrubbo@gmail.com>
2021-01-25 17:14:51 -0500

    last few changes

--- assets/js/ApiTest.js
+++ assets/js/ApiTest.js
@@ -6,21 +6,23 @@ const lastFmSecret = "82409a163bce82966c42be3a35f3d950"
 //const youtubeKey = "AIzaSyDhTehhaCl-IoNtfuRz4ePLHX7iO98lljw" Kaitlyn's yt key
 
 // const youtubeKey = "AIzaSyAaQzZrnuJSEVUnyYXGYHcEKoluy22eyu0" //Javier's 1st api key
-const youtubeKey = "AIzaSyBsUSXEw1JU8u8efirUOm8mhbuVJD3YZVE" //Javier 2nd test key
+ const youtubeKey = "AIzaSyBsUSXEw1JU8u8efirUOm8mhbuVJD3YZVE" //Javier 2nd test key
 //const youtubeKey ="AIzaSyA3bUZtYtaKf3MzhjEqjf3RznY_1E2LuQY" //Cole's yt api key
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 });
 
-
-$(document).ready(function musicNote() {
+function findArtist() {
     //variable for # of results returned
     var resLength = "1"
     //for artist search need + instead of " " --> loop?
     //var lfmartist = $(".input");  //"the beatles" //$(".input")
     //need to work on jquery to save input from form and submit on btn press
-    var lfmartist = ""
+
+    var lfmartist = document.getElementById("artistInput").value
+    console.log("lfmartist is:" + lfmartist)
+
     // console.log(lfmartist);
     //needs on click jquery somewhere around here
 
@@ -68,7 +70,6 @@ $(document).ready(function musicNote() {
     //test to see if it shuffles
     //console.log(ytubeSTerm);
 
-
     //Youtube api seperate component, can be moved
     ytubeSTerm.forEach(function (yQueryLoop) {
         console.log(yQueryLoop);
@@ -86,12 +87,12 @@ $(document).ready(function musicNote() {
             console.log("ytReturnVidId is: " + ytReturnVidId);
 
             //placeholder for video id 
-            // var ytReturnVidId = "1V_xRb0x9aw"
+           // var ytReturnVidId = "1V_xRb0x9aw"
 
             //pl for vid title
             //var ytReturnVidTitle = "Example Title"
             //retrieve video title 
-            var ytReturnVidTitle = ytResponse.items[0].snippet.title
+           var ytReturnVidTitle = ytResponse.items[0].snippet.title
             console.log(ytReturnVidTitle);
 
             //retrieve video thumbnails
@@ -102,53 +103,52 @@ $(document).ready(function musicNote() {
 
             //append to document with above results, use comboResults as inside html text
             $("#ytResults").append(comboResults);
-            localStorage.clear()
+localStorage.clear()
             return ytReturnVidId
 
         });
     });
 };
-
-var tag = document.createElement('script');
-
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'NdSMeBrNp4A',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
+ 
+   /* var tag = document.createElement('script');
 
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    $('#player').show();
-    event.target.playVideo();
-}
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 5. The API calls this function when the player's state changes.
+    // 3. This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+    var player;
 
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING) {
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'NdSMeBrNp4A',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
     }
-}
 
 
-function stopVideo() {
-    player.stopVideo();
-}
+    // 4. The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+        $('#player').show();
+        event.target.playVideo();
+    }
+
+    // 5. The API calls this function when the player's state changes.
+
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING) {
+        }
+    }
+
+
+    function stopVideo() {
+        player.stopVideo();
+    } */
 //YOUTUBE
-
